78d3f75798254226cbf51cbd97ceddde7b7b278a
Vlad <gnatrusyy@gmail.com>
2025-12-19 17:14:57 +0200

    add On-demand rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vlad-fedorenko-93.github.io',
   integrations: [react(), markdoc(), keystatic(), svelte()],
+  adapter: netlify(),
 });
