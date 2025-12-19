4a8a430a04d74a9274205c86e9307ff1bf7c2af3
Vlad <gnatrusyy@gmail.com>
2025-12-19 17:26:55 +0200

    remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,9 @@ import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import svelte from '@astrojs/svelte';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://vlad-fedorenko-93.github.io',
   integrations: [react(), markdoc(), keystatic(), svelte()],
-  adapter: netlify(),
 });
