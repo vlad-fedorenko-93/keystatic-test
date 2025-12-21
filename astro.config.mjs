daec375975cba493ab0ba98c0cac2cc26855dfe4
Vlad <gnatrusyy@gmail.com>
2025-12-21 17:39:15 +0200

    changes trailing stash to always

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   output: 'static',
   site: 'https://vlad-fedorenko-93.github.io',
-  trainlingStash: 'never',
+  trainlingStash: 'always',
   integrations: [react(), markdoc(), svelte(), process.env.NODE_ENV !== 'production' ? keystatic() : []].flat(),
   vite: {
     plugins: [tailwindcss()],
