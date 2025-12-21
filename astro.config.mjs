032c52fc83bf612df97cdb08fc4dc5a35f2dd3da
Vlad <gnatrusyy@gmail.com>
2025-12-21 17:20:12 +0200

    added traingStash property into astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   output: 'static',
   site: 'https://vlad-fedorenko-93.github.io',
+  trainlingStash: 'never',
   integrations: [react(), markdoc(), svelte(), process.env.NODE_ENV !== 'production' ? keystatic() : []].flat(),
   vite: {
     plugins: [tailwindcss()],
